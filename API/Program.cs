33bcc3c147a5d869edce88f5da44072fd8f8ac12
lsciencia <luissciencia.t.i@gmail.com>
2023-11-02 16:39:43 -0300

    building a walking skeleton ANGULAR

--- API/Program.cs
+++ API/Program.cs
@@ -10,15 +10,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder
+.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins("https://localhost:4200"));
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7167/api/users')
+      .subscribe({
+        next: (response) => {
+          this.users = response;
+        },
+        error: (err) => {
+          console.log(err)
+        },
+        complete: () => {
+
+        }
+      });
+  }
+
+}
